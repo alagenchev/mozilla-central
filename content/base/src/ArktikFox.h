90493a7080c55bd5f73125fe1f32df7ff3cb5688
Ivan Alagenchev <alagenchev@gmail.com>
2013-10-29 22:42:28 -0400

    adding main code that decides based on sqlite db whether a domain is financial or not, using any sqlite paths, not only special dirs

--- content/base/src/ArktikFox.h
+++ content/base/src/ArktikFox.h
@@ -1,55 +1,177 @@
+#ifndef alagenchev_fox_
+#define alagenchev_fox_
 #include <sqlite3.h>
+#include <mutex>
+#include <mozIStorageConnection.h>
+#include "mozilla/storage.h"
+#include "nsAppDirectoryServiceDefs.h"
+#include <unistd.h>
+
+class mozIStorageService;
 
 namespace alagenchev {
+
     enum DomainType {
         eFinancialDomain = 0x00,
         eOtherDomain = 0x01
     };
 
+    static std::mutex mtx;
+
     class ArktikFox {
         public:
-            static int GetDomainType(nsCOMPtr<nsIURI> aURI, DomainType *type) {
-                sqlite3 *db;
-                int result;
+            static nsresult IsThirdPartyContent(nsCOMPtr<nsIURI> aRequestingLocation,
+                    nsCOMPtr<nsIURI> aContentLocation, bool &aIsThirdPartyContent)
+            {
+                nsAutoCString requestingLocationSpec;
+                aRequestingLocation->GetSpec(requestingLocationSpec);
+
+                nsCOMPtr<nsIFile> dbFile;
+                nsresult rv = getDBFile(getter_AddRefs(dbFile));
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                nsCOMPtr<mozIStorageService> dbService =
+                    do_GetService("@mozilla.org/storage/service;1", &rv);
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                nsCOMPtr<mozIStorageConnection> dbConn;
+                rv = dbService->OpenDatabase(dbFile, getter_AddRefs(dbConn));
+
+                if(NS_FAILED(rv))
+                {
+                    fprintf(stderr, "ARKTIK FOX FAILED 1 \n");
+                }
+                NS_ENSURE_SUCCESS(rv, rv);
+                nsAutoCString query;
+                query.Assign("select id from websites where domain='");
+                query.Append(requestingLocationSpec);
+                query.Append("';");
+
+                nsCOMPtr<mozIStorageStatement> statement;
+                rv = dbConn->CreateStatement(query, getter_AddRefs(statement));
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                PRInt32 site_id = -1;
+                bool hasMoreData;
+                if (NS_SUCCEEDED(statement->ExecuteStep(&hasMoreData)) && hasMoreData) {
+                    rv = statement->GetInt32(0, &site_id);
+                    NS_ENSURE_SUCCESS(rv, rv);
+                }
+
+                statement->Reset();
+
+                query.Assign("SELECT * FROM not_third_party where owner ='");
+                query.AppendInt(site_id, 10);
+                query.Append("';");
+
+
+                rv = dbConn->CreateStatement(query, getter_AddRefs(statement));
+                nsAutoString domain;
+                nsAutoCString contentLocationSpec;
+                aContentLocation->GetSpec(contentLocationSpec);
+                nsAutoCString contentHost;
+                aContentLocation->GetHost(contentHost);
+
+                while(NS_SUCCEEDED(statement->ExecuteStep(&hasMoreData)) && hasMoreData) {
+                    rv = statement->GetString(1, domain);
+                    NS_ENSURE_SUCCESS(rv, rv);
+                    nsAutoCString temp;
+                    temp.Assign(NS_ConvertUTF16toUTF8(domain).get());
+                    if(contentHost.Find(temp.get(), true, 0, -1) > -1) {
+                        aIsThirdPartyContent = false;
+                        statement->Finalize();
+                        dbConn->Close();
+                        return NS_OK;
+                    }
+
+                }
+                aIsThirdPartyContent = true;
+                statement->Finalize();
+                dbConn->Close();
+
+
+                return NS_OK;
+            }
+
+            static nsresult GetDomainType(nsCOMPtr<nsIURI> aURI, DomainType *type) {
+
+                nsresult rv;
                 nsAutoCString spec;
                 aURI->GetSpec(spec);
 
-                result = sqlite3_open("/home/alagenchev/mozilla-central/database/websites.sqlite", &db);
-                if( result ){
-                    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-                    sqlite3_close(db);
-                    return 1;
-                }
                 nsAutoCString query;
                 query.Assign("select count(1) as COUNT from websites where domain='");
                 query.Append(spec);
                 query.Append("';");
 
-                sqlite3_stmt *selectStmt;
-                sqlite3_prepare(db, query.get(), query.Length() + 1, &selectStmt, NULL);
+                nsCOMPtr<mozIStorageService> dbService =
+                    do_GetService("@mozilla.org/storage/service;1", &rv);
+                NS_ENSURE_SUCCESS(rv, rv);
 
-                int count = 0;
-                int step_result = sqlite3_step (selectStmt);
-                if (step_result == SQLITE_ROW) {
-                    count = sqlite3_column_int(selectStmt, 0);
+                nsCOMPtr<nsIFile> dbFile;
+                rv = getDBFile(getter_AddRefs(dbFile));
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                nsCOMPtr<mozIStorageConnection> dbConn;
+                rv = dbService->OpenDatabase(dbFile, getter_AddRefs(dbConn));
+
+                if(NS_FAILED(rv))
+                {
+                    fprintf(stderr, "ARKTIK FOX FAILED 2 \n");
+                }
+                nsCOMPtr<mozIStorageStatement> statement;
+                rv = dbConn->CreateStatement(query, getter_AddRefs(statement));
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                bool hasMoreData;
+                if (NS_SUCCEEDED(statement->ExecuteStep(&hasMoreData)) && hasMoreData) {
+                    PRInt32 value;
+                    rv = statement->GetInt32(0, &value);
+                    NS_ENSURE_SUCCESS(rv, rv);
+                    if(value > 0) {
+                        *type = eFinancialDomain;
+                    }
+                    else {
+                        *type = eOtherDomain;
+                    }
 
                 }
-                else {
+                statement->Finalize();
+                dbConn->Close();
 
-                    sqlite3_close(db);
-                    return 0;
+                return NS_OK;
+            }
+
+        private:
+            static nsresult getDBFile(nsIFile **aOutDBFile) {
+                NS_ENSURE_ARG_POINTER(aOutDBFile);
+                *aOutDBFile = nullptr;
+
+                char cwd[2500];
+                char* result = getcwd(cwd, sizeof(cwd));
+                if ( result == NULL)
+                {
+                    return NS_ERROR_FAILURE;
                 }
 
+                nsAutoString currentDir;
+                currentDir.Append(NS_ConvertUTF8toUTF16(cwd));
 
-                if(count > 0) {
-                    *type = eFinancialDomain;
-                }
-                else {
-                    *type = eOtherDomain;
-                }
+                nsresult rv;
+                nsCOMPtr<nsIFile> dbFile = do_CreateInstance("@mozilla.org/file/local;1", &rv);
+                NS_ENSURE_SUCCESS(rv, rv);
 
-                sqlite3_close(db);
-                return 0;
+                rv = dbFile->InitWithPath(currentDir);
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                rv = dbFile->Append(NS_LITERAL_STRING("websites.sqlite"));
+                NS_ENSURE_SUCCESS(rv, rv);
+
+                NS_ADDREF(*aOutDBFile = dbFile);
+
+                return NS_OK;
+
             }
     };
 }
+#endif
